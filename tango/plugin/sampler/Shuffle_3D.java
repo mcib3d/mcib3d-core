1e931f22d1a1d2a9350a7c64d2a849dd80e7062a
thomas boudier <thomas.boudier@upmc.fr>
2014-12-08 15:48:36 +0800

    Keep holes when binary close

--- tango/plugin/sampler/Shuffle_3D.java
+++ tango/plugin/sampler/Shuffle_3D.java
@@ -1,18 +1,12 @@
 package tango.plugin.sampler;
 
+import ij.IJ;
 import ij.ImagePlus;
-import ij.Prefs;
-import ij.gui.GenericDialog;
-import ij.plugin.filter.PlugInFilter;
-import ij.process.ImageProcessor;
 import mcib3d.geom.Object3D;
 import mcib3d.geom.Object3DVoxels;
 import mcib3d.geom.Objects3DPopulation;
-import mcib3d.geom.Vector3D;
 import mcib3d.image3d.ImageHandler;
 import mcib3d.image3d.ImageInt;
-import mcib3d.image3d.ImageShort;
-import mcib3d.image3d.processing.FastFilters3D;
 import tango.dataStructure.InputCellImages;
 import tango.dataStructure.SegmentedCellImages;
 import tango.parameter.*;
@@ -47,84 +41,85 @@ import tango.parameter.*;
  * @author thomas
  */
 public class Shuffle_3D implements Sampler {
-    // FIXME plutot interface sampler 
 
     ImagePlus plus;
     private boolean debug;
     double ang = 0;
     String[] axes = {"X-axis", "Y-axis", "Z-axis"};
     int axe = 0;
-    
+
     // parameters
     StructureParameter channelMask = new StructureParameter("Channel for mask:", "shuffle_mask", 0, true);
     StructureParameter channelObjects = new StructureParameter("Channel for objects:", "shuffle_objects", 1, true);
-    BooleanParameter rotate = new BooleanParameter("Rotation:", "shuffle_rotate", false);
-    DoubleParameter angle = new DoubleParameter("Angle:", "shuffle_angle", ang, Parameter.nfDEC2);
-    ChoiceParameter axechoice = new ChoiceParameter("Axe:", "shuffle_axe", axes, axes[axe]);
-    TextParameter testtext = new TextParameter("Test:", "shuffle_test", "test de texte");
-    SpinnerParameter slid = new SpinnerParameter("test slider:", "shuffle_test2", 0, 10, 5);
-    Parameter[] parameters = {channelMask, channelObjects, rotate, angle, axechoice, testtext, slid};
+    //BooleanParameter rotate = new BooleanParameter("Rotation:", "shuffle_rotate", false);
+    //DoubleParameter angle = new DoubleParameter("Angle:", "shuffle_angle", ang, Parameter.nfDEC2);
+    //ChoiceParameter axechoice = new ChoiceParameter("Axe:", "shuffle_axe", axes, axes[axe]);
+    //TextParameter testtext = new TextParameter("Test:", "shuffle_test", "test de texte");
+    //SpinnerParameter slid = new SpinnerParameter("test slider:", "shuffle_test2", 0, 10, 5);
+    Parameter[] parameters = {channelMask, channelObjects};
     Objects3DPopulation population;
+    ImageHandler draw = null;
 
     boolean verbose;
-    int nbCPUs=1;
+    int nbCPUs = 1;
+
     @Override
     public void setMultithread(int nbCPUs) {
-        this.nbCPUs=nbCPUs;
+        this.nbCPUs = nbCPUs;
     }
-    
+
     @Override
     public void setVerbose(boolean verbose) {
-        this.verbose=verbose;
-    }
-    
-    public int setup(String arg, ImagePlus imp) {
-        plus = imp;
-
-        return PlugInFilter.DOES_16 + PlugInFilter.DOES_8G;
+        this.verbose = verbose;
     }
 
-    public void run(ImageProcessor ip) {
-        debug = true;
-        GenericDialog gd = Parameter.buildDialog("Shuffle", parameters);
-        gd.showDialog();
-        ang = gd.getNextNumber();
-        axe = gd.getNextChoiceIndex();
-        // test shuffle
-        ImageShort shu = new ImageShort("shuffle",plus.getWidth(), plus.getHeight(), plus.getStackSize());
-        shu.addValue(1);
-        
-        if (debug) {
-            shuffle(plus, shu.getImagePlus()).show();
-        }
-    }
+//    public int setup(String arg, ImagePlus imp) {
+//        plus = imp;
+//
+//        return PlugInFilter.DOES_16 + PlugInFilter.DOES_8G;
+//    }
+//
+//    public void run(ImageProcessor ip) {
+//        debug = true;
+//        GenericDialog gd = Parameter.buildDialog("Shuffle", parameters);
+//        gd.showDialog();
+//        ang = gd.getNextNumber();
+//        axe = gd.getNextChoiceIndex();
+//        // test shuffle
+//        ImageShort shu = new ImageShort("shuffle", plus.getWidth(), plus.getHeight(), plus.getStackSize());
+//        shu.addValue(1);
+//
+//        if (debug) {
+//            shuffle(plus, shu.getImagePlus()).show();
+//        }
+//    }
 
-    private ImagePlus shuffle(ImagePlus spotsplus, ImagePlus maskplus) {
-        Objects3DPopulation pop = new Objects3DPopulation();
-        pop.addImage(spotsplus);
-        //IntImage3D draw = new IntImage3D(plus.getWidth(), plus.getHeight(), plus.getStackSize());
-        ImageInt draw = new ImageShort("draw", plus.getWidth(), plus.getHeight(), plus.getStackSize());
-        Object3D mask = new Object3DVoxels(maskplus, 1);
-        pop.setMask(mask);
-        Vector3D axis = new Vector3D();
-        if (axe == 0) {
-            axis.setCoord(1, 0, 0);
-        } else if (axe == 1) {
-            axis.setCoord(0, 1, 0);
-        } else if (axe == 2) {
-            axis.setCoord(0, 0, 1);
-        }
-        if (pop.shuffle(ang, axis)) {
-            pop.draw(draw.getImageStack());
-            draw = (ImageInt)FastFilters3D.filterIntImage(draw, FastFilters3D.MAX, 1.5f, 1.5f, 1.5f, Prefs.getThreads(), false);
-            draw = (ImageInt)FastFilters3D.filterIntImage(draw, FastFilters3D.MIN, 1.5f, 1.5f, 1.5f, Prefs.getThreads(), false);
-            //draw.filterGeneric(draw2, 1.5f, 1.5f, 1.5f, 0, draw.getSizez(), IntImage3D.FILTER_MAX);
-            //draw2.filterGeneric(draw, 1.5f, 1.5f, 1.5f, 0, draw.getSizez(), IntImage3D.FILTER_MIN);
-            return new ImagePlus("shuffle", draw.getImageStack());
-        } else {
-            return null;
-        }
-    }
+//    private ImagePlus shuffle(ImagePlus spotsplus, ImagePlus maskplus) {
+//        Objects3DPopulation pop = new Objects3DPopulation();
+//        pop.addImage(spotsplus);
+//        //IntImage3D draw = new IntImage3D(plus.getWidth(), plus.getHeight(), plus.getStackSize());
+//        draw = new ImageShort("draw", plus.getWidth(), plus.getHeight(), plus.getStackSize());
+//        Object3D mask = new Object3DVoxels(maskplus, 1);
+//        pop.setMask(mask);
+//        Vector3D axis = new Vector3D();
+//        if (axe == 0) {
+//            axis.setCoord(1, 0, 0);
+//        } else if (axe == 1) {
+//            axis.setCoord(0, 1, 0);
+//        } else if (axe == 2) {
+//            axis.setCoord(0, 0, 1);
+//        }
+//        if (pop.shuffle(ang, axis)) {
+//            pop.draw(draw);
+//            //draw.show("test shuffle");
+//            //draw = (ImageInt) FastFilters3D.filterIntImage(draw, FastFilters3D.CLOSEGRAY, 1.5f, 1.5f, 1.5f, Prefs.getThreads(), false);
+//            //draw.filterGeneric(draw2, 1.5f, 1.5f, 1.5f, 0, draw.getSizez(), IntImage3D.FILTER_MAX);
+//            //draw2.filterGeneric(draw, 1.5f, 1.5f, 1.5f, 0, draw.getSizez(), IntImage3D.FILTER_MIN);
+//            return new ImagePlus("shuffle", draw.getImageStack());
+//        } else {
+//            return null;
+//        }
+//    }
 
     @Override
     public Parameter[] getParameters() {
@@ -133,29 +128,30 @@ public class Shuffle_3D implements Sampler {
 
     @Override
     public void initSampler(InputCellImages raw, SegmentedCellImages seg) {
-        ImagePlus spotPlus = channelObjects.getImagePlus(seg, false);
-        ImagePlus maskPlus = channelMask.getImagePlus(seg, false);
-
-        population = new Objects3DPopulation();
-        population.addImage(spotPlus);
+        ImageInt spotPlus = seg.getImage(channelObjects.getIndex());//channelObjects.getImagePlus(seg, false);
+        ImageInt maskPlus = seg.getImage(channelMask.getIndex());//channelMask.getImagePlus(seg, false);
+        IJ.log("Shuffling objects");
+        population = new Objects3DPopulation(spotPlus);
         Object3D mask = new Object3DVoxels(maskPlus, 1);
         population.setMask(mask);
+        population.shuffle(0, null);
+        draw = spotPlus.createSameDimensions();
+        population.draw(draw);
     }
 
     @Override
     public Object3D[] getSample() {
-        population.shuffle(0, null);
         return population.getObjectsArray();
     }
 
     @Override
     public String getHelp() {
-        return "";
+        return "Shuffle the objects positions. One object will be located at another object location. ";
     }
-    
+
     @Override
     public void displaySample() {
-        //shuffle(plus, shu.getImagePlus()).show();
+        draw.show("Shuffle");
     }
 
 }
