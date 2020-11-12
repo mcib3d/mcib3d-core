7f7a1b513df52f2ca2289f64246e08c599e42c67
mcib3d <thomas.boudier@upmc.fr>
2020-11-12 15:57:11 +0100

    update

--- src/main/java/mcib3d/geom/interactions/InteractionsCompute.java
+++
@@ -1,7 +0,0 @@
-package mcib3d.geom.interactions;
-
-import mcib3d.image3d.ImageHandler;
-
-public interface InteractionsCompute {
-    InteractionsList compute(ImageHandler image);
-}
--- src/main/java/mcib3d/utils/Logger/AbstractLog.java
+++
@@ -1,8 +0,0 @@
-package mcib3d.utils.Logger;
-
-/**
- * Created by thomasb on 15/11/16.
- */
-public abstract class AbstractLog {
-    abstract public void log(String S);
-}
